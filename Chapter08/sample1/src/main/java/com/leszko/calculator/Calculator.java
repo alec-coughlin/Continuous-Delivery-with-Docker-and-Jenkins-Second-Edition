7b6196370ef29190e1fcb7e4e108e49ca6ec49b2
alec-coughlin <119661906+alec-coughlin@users.noreply.github.com>
2025-02-24 23:44:21 -0800

    Update Calculator.java

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -2,11 +2,11 @@ package com.leszko.calculator;
 
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-
-@Service
 /**
-*javadoc comment
+*javadoc 
+*comment
 */
+@Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
 	@Cacheable("sum")
